538874a3b1d3d780a91ca133ad77f0d9f83c9e65
beyzakoser <b.koser@hotmail.com>
2020-08-01 23:00:37 +0300

    signin

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const { Sequelize, Model } = require("sequelize");
-
+const express = require('express');
+const cors = require('cors');
+const _ = require('underscore');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 const sequelize = new Sequelize('intibakVeMatbu', 'root', '12345678', {
     host: 'localhost',
     dialect: 'mysql',
@@ -87,6 +96,99 @@ bolumdersleri.belongsTo(fsmvuders, {
     foreignKey: "fsmvudersId"
 });
 
+///////////////////
+
+app.get('/admin', (req, res) => {
+    admin.findAll({
+        raw: true
+    }).then(c => {
+        //res.send(c)
+        //res.redirect("http://localhost:3000/dashboard/")
+        //console.log(c);
+
+    }).catch(err => console.log("Error : ", err));
+
+});
+
+app.post('/ekle', (req, res) => {
+    const { mail, sifre } = req.body;
+    //var deger=_.pick(req.body,"mail"); //obje tipinde geliyor..
+    console.log(mail);
+    admin.findAll({
+        where: {
+            kullaniciAdi: mail,
+            sifre: sifre
+        },
+        raw: true
+
+    }).then(
+         c => {
+         console.log(c.length)
+         if (c.length != 0) { res.send("success") }
+
+         });
+});
+/*
+app.get('/dersler', function (req, res) {
+    sequelize.models.fsmvuders.findAll({
+        where: {
+            grupBilgisi: "BLM AS VII"
+        },
+        raw: true
+    }).then(function (rows) {
+        //console.log(rows)
+        res.send(rows)
+
+
+    });
+  });
+
+  app.get('/ogretimuyleri', function (req, res) {
+    sequelize.models.fsmvuders.findAll({
+        where: {
+            grupBilgisi: "BLM AS VII"
+        },
+        raw: true
+    }).then(function (rows) {
+        //console.log(rows)
+        res.send([{"maya":"mnsmn"}])
+
+
+    });
+  });
+ //önce bu path e geliyor oradan veri tabanına kaydediyorum
+  app.post("/dersEkle", (req, res) => {
+    console.log(
+      "req.body***********************************************",
+      req.body
+    );
+    Event_classroom.create(req.body.evtClassroom)
+      .then(evtClassroom => {
+        console.log(evtClassroom.dataValues);
+        res.send(evtClassroom.dataValues);
+      })
+      .catch(err => {
+        console.log("error ", err);
+      });
+  });
+    app.post("/ogretim", (req, res) => {
+    console.log(
+      "req.body***********************************************",
+      req.body
+    );
+    Event_classroom.create(req.body.evtClassroom)
+      .then(evtClassroom => {
+        console.log(evtClassroom.dataValues);
+        res.send(evtClassroom.dataValues);
+      })
+      .catch(err => {
+        console.log("error ", err);
+      });
+  });*/
+app.listen(3004, () => {
+    console.log("server is listening");
+});
+/*
 sequelize.sync({ force: false }).then(() => {
     console.log("basarili bir şekilde bağlandı");
     /* sequelize.models.fsmvuders.create({
@@ -110,7 +212,7 @@ sequelize.sync({ force: false }).then(() => {
         } else {
             console.log("silindi")
         }
-    })*/
+    })
     sequelize.models.fsmvuders.findAll({
         where: {
             grupBilgisi: "BLM AS VII"
@@ -122,5 +224,5 @@ sequelize.sync({ force: false }).then(() => {
     });
 
 
-});
+});*/
 

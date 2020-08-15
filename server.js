529e1f58080aa55de0474cf021baa45b984a87d9
Beyza <b.koser@hotmail.com>
2020-08-15 05:51:21 +0300

    /basvuru kısmı eklendi

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const _ = require('underscore');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -58,30 +59,44 @@ app.get('/dersler', (req, res) => {
 
 
 });
+
 app.post('/basvuru', (req, res) => {
-    //const { ad, soyad, fakulte, bolum, mail, universite, girisYil, basvuruTur } = req.body;
-    //console.log(ad, soyad, fakulte, bolum, mail, universite, girisYil, basvuruTur);
-   // console.log(req.body);
-
-
+    const person = req.body;
+    const date = new Date();
+    var dateTime = date.toISOString().slice(0, 10);
+    ogrenci.create({
+        ogrenciAd: person.name,
+        ogrenciSoyad: person.lastname,
+        ogrenciMail: person.mail,
+        ogrenciFakulte: person.faculty,
+        ogrenciBolum: person.department,
+        universiteAdi: person.university,
+        girisYil: person.year,
+        talepTarih: dateTime
+    }).catch(err => console.log("Error : ", err));
 });
+
+
+
 app.post('/dersDuzenle', (req, res) => {
     //client tarafından değişen verilerin hepsi tek bir objede geliyor.
-    const  {inserts}  = req.body[0];
-    const  {updates}  = req.body[1];
-    const  {deletes}  = req.body[2];
+    const { inserts } = req.body[0];
+    const { updates } = req.body[1];
+    const { deletes } = req.body[2];
+
+
     // update kısmı 
     updates.forEach(eleman => {
         fsmvuders.update({
             dersKodu: eleman.dersKodu,
             dersAd: eleman.dersAd,
-            kredi:eleman.kredi,
+            kredi: eleman.kredi,
             akts: eleman.akts,
             teoriSaat: eleman.teoriSaat,
-            labSaat:eleman.labSaat,
+            labSaat: eleman.labSaat,
             kontenjan: eleman.kontenjan,
             teoriOnline: eleman.teoriOnline,
-            labOnline:eleman.labOnline
+            labOnline: eleman.labOnline
         }, {
             where: {
                 id: eleman.id
@@ -93,15 +108,47 @@ app.post('/dersDuzenle', (req, res) => {
         fsmvuders.create({
             dersKodu: eleman.dersKodu,
             dersAd: eleman.dersAd,
-            kredi:eleman.kredi,
+            kredi: eleman.kredi,
             akts: eleman.akts,
             teoriSaat: eleman.teoriSaat,
-            labSaat:eleman.labSaat,
+            labSaat: eleman.labSaat,
             kontenjan: eleman.kontenjan,
             teoriOnline: eleman.teoriOnline,
-            labOnline:eleman.labOnline
+            labOnline: eleman.labOnline
         }).catch(err => console.log("Error : ", err));
-    });
+        // fsmvuders.findOne({
+        //     where: {
+        //         dersKodu: eleman.dersKodu ,
+        //         dersAd: eleman.dersAd,
+        //         kredi: eleman.kredi,
+        //         akts:eleman.akts,
+        //         teoriSaat: eleman.teoriSaat,
+        //         labSaat: eleman.labSaat,
+        //     }
+
+        // }).then(res => {
+        //     if (res !== null) {
+        //         console.log("boş değil");
+        //     }
+        //     else {
+        //         fsmvuders.create({
+        //             dersKodu: eleman.dersKodu,
+        //             dersAd: eleman.dersAd,
+        //             kredi: eleman.kredi,
+        //             akts: eleman.akts,
+        //             teoriSaat: eleman.teoriSaat,
+        //             labSaat: eleman.labSaat,
+        //             kontenjan: eleman.kontenjan,
+        //             teoriOnline: eleman.teoriOnline,
+        //             labOnline: eleman.labOnline
+        //         }).catch(err => console.log("Error : ", err));
+
+
+        //     }
+        // }
+        // ).catch(err => console.log("Error : ", err));
+    })
+
     //delete
     deletes.forEach(eleman => {
         fsmvuders.destroy({
@@ -112,6 +159,7 @@ app.post('/dersDuzenle', (req, res) => {
     });
 
 });
+
 
 app.listen(3004, () => {
     console.log("server is listening");

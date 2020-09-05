8b8219848a96a9dd78712c69e5a60119d5781a78
Beyza <b.koser@hotmail.com>
2020-09-05 03:57:03 +0300

    relationlar değişti ve  basvuru servisi yazıldı

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 //const { Sequelize, Model } = require("sequelize");
 const express = require('express');
 const cors = require('cors');
-const _ = require('underscore');
-
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -57,50 +55,6 @@ app.get('/dersler', (req, res) => {
                 res.send(c)
             }).catch(err => console.log("Error : ", err));
 
-
-});
-
-app.post('/basvuru', (req, res) => {
-    const person = req.body;
-    const date = new Date();
-    var dateTime = date.toISOString().slice(0, 10);
-    ogrenci.create({
-        ogrenciAd: person.ogrenciAd,
-        ogrenciSoyad: person.ogrenciSoyad,
-        ogrenciMail: person.ogrenciMail,
-        ogrenciFakulte: person.ogrenciFakulte,
-        ogrenciBolum: person.ogrenciBolum,
-        universiteAdi: person.universiteAdi,
-        girisYil: person.girisYil,
-        talepTarih: dateTime,
-        basvuruTur: person.basvuruTur,
-        // ogrenciId:{
-        // include:[{
-        //     model:ogrencidersleri,
-        //     as:"ogrencidersleri",
-        //     //foreignKey:"ogrenciId"
-        // }]}
-
-    }).catch(err => console.log("Error : ", err));
-    
-    // ogrencidersleri.create(req.body.dersler,{
-    //    include:[{
-    //                 model:ogrenci,
-    //                 as:"ogrenci"
-    //             }]
-
-    // }).catch(err => console.log("Error : ", err));
-    // req.body.dersler.forEach(eleman => {
-    //     ogrencidersleri.create({
-    //         dersKodu:eleman.derskodu,
-    //         dersAdi:eleman.dersadi,
-    //         kredi:eleman.kredi,
-    //         akts:eleman.akts,
-    //         basariNotu:eleman.basarinotu,
-
-        
-    // }).catch(err => console.log("Error : ", err));
-    // })
 });
 
 
@@ -176,9 +130,53 @@ app.get('/ogretimElemanlari', (req, res) => {
 
 
 });
+app.post('/akademisyenDuzenle', (req, res) => {
+    //client tarafından değişen verilerin hepsi tek bir objede geliyor.
+    const { inserts } = req.body[0];
+    const { updates } = req.body[1];
+    const { deletes } = req.body[2];
+    // update kısmı 
+    updates.forEach(eleman => {
+        ogretimelemani.update({
+            unvan: eleman.unvan,
+            ad: eleman.ad,
+            soyad: eleman.soyad,
+            statu: eleman.statu,
+
+        }, {
+            where: {
+                id: eleman.id
+            }
+        }).catch(err => console.log("Error : ", err));
+    });
+    //insert
+    inserts.forEach(eleman => {
+        ogretimelemani.create({
+            unvan: eleman.unvan,
+            ad: eleman.ad,
+            soyad: eleman.soyad,
+            statu: eleman.statu,
+        }).catch(err => console.log("Error : ", err));
+
+    })
+    //delete
+    deletes.forEach(eleman => {
+        ogretimelemani.destroy({
+            where: {
+                id: eleman.id
+            }
+        }).catch(err => console.log("Error : ", err));
+    });
+
+});
 app.get('/basvurulistesi', (req, res) => {
-    ogrenci.findAll({  
+    ogrenci.findAll({
+        //tarihe göre en yeni tarih üstte görünecek şekilde  gönderdim.
+        order: [
+            ['talepTarih', 'DESC'],
+        ],
         attributes: [
+            "id",
             "ogrenciAd",
             "ogrenciSoyad",
             "ogrenciMail",
@@ -191,34 +189,93 @@ app.get('/basvurulistesi', (req, res) => {
         ],
         raw: true
     }).then(
-            c => {
-                res.send(c)
-            }).catch(err => console.log("Error : ", err));
+        c => {
+            res.send(c)
+        }).catch(err => console.log("Error : ", err));
 
 });
-// app.get('/guzDersleri', (req, res) => {
-//     fsmvuders.findAll({
-//         //dersAdına göre alfabetik sıra ile gönderdim.
-//         order: [
-//             ['dersAd', 'ASC'],
-//         ],
-//         //hangi özelliklerin gitmesini istiyorsam;
-//         attributes: [
-//             "id",
-//             "dersAd",
-//         ],
-//         where:{
-//             donem:'Güz'
 
-//         },
-//         raw: true
-//     })
-//         .then(
-//             c => {
-//                 res.send(c)
-//             }).catch(err => console.log("Error : ", err));
-// });
+app.post('/basvuru', (req, res) => {
+    //bu kısımda intibak başvurusu yapan öğrenci ve öğrenci dersleri ekleniyor.
+    //include kısmı öğrencidersleri tablosunda öğrenciId olduğundan dolayı gerekli
+    ogrenci.create(req.body, {
+        include: [{
+            model: ogrencidersleri,
+            as: "ogrencidersleri",
+        }]
+    }).catch(err => console.log("Error : ", err));
+});
 
+let yil, mufredatAdi;
+app.get('/basvuruIncele/:id', (req, res) => {
+    let query = [], query2 = [], query3 = [];
+    //gelen ogrenci id sine göre dersleri ve giriş yılına göre
+    //müfredatı gönderiyorum.
+    ogrencidersleri.findAll({
+        where: { ogrenciId: req.params.id },
+        attributes: [
+            "dersKodu",
+            "dersAdi",
+            "kredi",
+            "akts",
+            "basariNotu"
+        ],
+        raw: true,
+
+    }).then(
+        c => {
+            query.push(c)
+        }).catch(err => console.log("Error : ", err));
+
+    ogrenci.findByPk(req.params.id, {
+        attributes: [
+            "universiteAdi",
+            "girisYil"
+        ],
+    }).then(
+        c => {
+            yil = c.girisYil;
+            if (yil < 2015) {
+                mufredatAdi = '2014-2015';
+                
+            } else if (yil >= 2015 && yil < 2019) {
+                mufredatAdi = '2015-2016';
+                
+            } else if (yil >= 2019) {
+                mufredatAdi = '2019-2020';
+                
+            }
+            query2 = { universiteAdi: c.universiteAdi }
+            query.push(query2)
+        }).catch(err => console.log("Error : ", err));
+
+    fsmvuders.findAll({
+    attributes: [
+        "id",
+        "dersKodu",
+        "grupBilgisi",
+        "dersAd",
+        'kredi',
+        "akts",
+    ],
+        include: [{
+            model: mufredat,
+            as: "mufredat",
+            where: {
+                ad: mufredatAdi
+            },
+    }],
+
+}).then(
+    c => {
+        query3 =  c 
+        query.push(query3)
+        console.log(query);
+        res.send(query)
+        //console.log(query[2].dersler);
+    }).catch(err => console.log("Error : ", err));
+
+})
 
 app.listen(3004, () => {
     console.log("server is listening");
